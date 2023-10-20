b5a514b3e161c621a085a113b1cd83862c3d807d
StalkersGlue <_@_>
2023-10-20 13:35:18 +0200

    remove dist files

--- src/main/client/src/generations.d.ts
+++
@@ -1,8 +0,0 @@
-import { LitElement } from 'lit';
-export declare class Generations extends LitElement {
-    static styles: import("lit").CSSResult;
-    private generations;
-    private websocket;
-    connectedCallback(): void;
-    render(): import("lit-html").TemplateResult<1>[];
-}
