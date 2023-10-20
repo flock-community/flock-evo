f9f127e442da0cb44f356fc0ba6965f081eb09bb
Onne Dallinga <onnedallinga@gmail.com>
2023-10-20 15:58:15 +0200

    Add autoplayer

--- src/main/client/src/flockEvoPlayer.ts
+++ src/main/client/src/flockEvoPlayer.ts
@@ -31,6 +31,32 @@ export class FlockEvoPlayer extends LitElement {
   private generations: GenerationView[] | undefined
 
   private currentGenerationAge = 0;
+  private playerStarted: boolean = false;
+
+  @property()
+  private delayTimer: number = 500;
+
+  startPlayer() {
+    if (this.playerStarted) {
+      return;
+    }
+    this.playerStarted = true;
+    this.playGeneration();
+  }
+
+  stopPlayer() {
+    this.playerStarted = false;
+  }
+
+  playGeneration() {
+    if (!this.playerStarted) {
+      return;
+    }
+    setTimeout(() => {
+      this.increaseWorldAge()
+      this.playGeneration()
+    }, this.delayTimer);
+  }
 
   connectedCallback() {
     super.connectedCallback();
@@ -49,6 +75,9 @@ export class FlockEvoPlayer extends LitElement {
   }
 
   private increaseWorldAge() {
+    if (!this.playerStarted) {
+      return;
+    }
     if (this.currentWorld && this.currentGeneration?.worlds[this.currentGenerationAge + 1]) {
       this.currentWorld = this.currentGeneration?.worlds[this.currentGenerationAge + 1];
       this.currentGenerationAge = this.currentGenerationAge + 1;
@@ -63,21 +92,29 @@ export class FlockEvoPlayer extends LitElement {
     }
   }
 
+  private changeDelayTimer(event: any): void {
+    console.log(event);
+    this.delayTimer = event.target.value;
+  }
+
   render() {
     return this.generations ? html`
-      <div class="evo-player__container">
-        <div class="evo-player__player">
-          Current age: ${this.currentWorld?.age}
+        <div class="evo-player__container">
+            <div class="evo-player__player">
+                Current age: ${this.currentWorld?.age}
+            </div>
+            <button @click="${this.increaseWorldAge}">+1</button>
+            <button @click="${this.decreaseWorldAge}">-1</button>
+            ${!this.playerStarted ? html`<button @click="${this.startPlayer}">Start autoplay</button>` : nothing}
+            ${this.playerStarted ? html`<button @click="${this.stopPlayer}">Stop autoplay</button>` : nothing}
+            <input type="number" step=100 min="100" value=500 @input=${this.changeDelayTimer}>
+            <select @input=${this.selectGeneration}>
+                ${this.generations.map((_, index) => html`
+                    <option value=${index}>${index}</option>
+                `)}
+            </select>
+            <flock-evo-world .world="${this.currentWorld}" class="evo-player__world"></flock-evo-world>
         </div>
-        <button @click="${this.increaseWorldAge}">+1</button>
-        <button @click="${this.decreaseWorldAge}">-1</button>
-        <select @input=${this.selectGeneration}>
-          ${this.generations.map((_, index) => html`
-            <option value=${index}>${index}</option>
-          `)}
-        </select>
-        <flock-evo-world .world="${this.currentWorld}" class="evo-player__world"></flock-evo-world>
-      </div>
     ` : nothing
   }
 
